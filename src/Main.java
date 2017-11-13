0abeead499ff82f0531817677634819c5c25ccca
Laimi <Laimil2012@gmail.com>
2017-11-13 19:11:41 +0200

    isProcessRunning method added plus comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import javafx.application.Platform;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,10 +12,15 @@ import java.util.Map;
  * Created by Andrew on 11/08/17.
  */
 public class Main {
-    public static void main(String[] args) {
 
+    private static final String TASKLIST = "tasklist";
+    private static final String KILL = "taskkill /F /IM";
+
+    public static void main(String[] args) {
+        String processName = "idea64.exe";
         String inputFolder = ".";
         String outputFolder = "output";
+
 
         if (args.length == 4) {
             if (args[0].equals("-input")) {
@@ -42,8 +49,16 @@ public class Main {
                 fileList.add(f);
             }
         }
-
         System.out.println(fileList.size());
+        try {
+            if(isProcessRunning(processName)!=false){
+                System.out.println("itWorks");
+            }else{
+                System.out.println("itNotWorks");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -51,4 +66,34 @@ public class Main {
         System.out.println("Invalid args. Use -input D:/path/to/the/input/folder/ -output D:/output/folder/path/");
         Platform.exit();
     }
+
+
+    /**
+     * This method searching given task in windows tasklist
+     * @param serviceName
+     * @return
+     * @throws Exception
+     */
+    private static boolean isProcessRunning(String serviceName)throws Exception{
+        Process p = Runtime.getRuntime().exec(TASKLIST);
+        BufferedReader reader = new BufferedReader( new InputStreamReader(
+                p.getInputStream()));
+        String line;
+        while ((line = reader.readLine())!=null){
+          //  System.out.println(line); //
+            if(line.contains(serviceName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * I guess programm need more permissions to kill other tasks
+     */
+//    private static void killProcess(String serviceName) throws Exception {
+//        Runtime.getRuntime().exec(KILL + serviceName);
+//    }
+
+
 }
