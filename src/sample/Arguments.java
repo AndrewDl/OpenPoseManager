6b9e5cd9b201e60b1f9ebc482e1c48c8f9be3bd6
Laimi <Laimil2012@gmail.com>
2017-12-07 14:28:32 +0200

    Arguments added

--- src/sample/Arguments.java
+++ src/sample/Arguments.java
@@ -1,5 +1,7 @@
 package sample;
 
+import java.util.Scanner;
+
 /**
  * Created by Laimi on 06.12.2017.
  */
@@ -7,23 +9,52 @@ public class Arguments {
     private String[] args;
     private String mode;
 public void setArgs(String[] args){
+  this.args = args;
+}
+private void internalTask(){
     if(args[0]==""){
+        Scanner sc = new Scanner("");
         System.out.println("No arguments found! Please, choose mode:");
         String s = sc.nextLine();
         if(s=="parallel"||s=="p"){
             System.out.println("Starting parallel mode...");
-
+            this.mode = "p";
         }
         if(s=="OpenPose"&&s=="op"){
             System.out.println("Starting OpenPose only mode...");
+            this.mode = "op";
         }
         if(s=="NewVision"&&s=="nv"){
             System.out.println("Starting NewVision only mode...");
+            this.mode = "nv";
         }
         if(s=="q"&&s=="exit"){
             System.exit(0);
         }
     }
+    if(args[1]=="parallel"||args[1]=="p"){
+        System.out.println("Starting parallel mode...");
+        this.mode = "p";
+    }else{
+    if(args[1]=="OpenPose"&&args[1]=="op"){
+        System.out.println("Starting OpenPose only mode...");
+        this.mode = "op";
+    }else{
+    if(args[1]=="NewVision"&&args[1]=="nv"){
+        System.out.println("Starting NewVision only mode...");
+        this.mode = "nv";
+    }else{
+    if(args[1]=="q"&&args[1]=="exit"){
+        System.exit(0);
+    }else{
+        System.out.println("No params found after -mode! Enter manually");
+        args[0]="";
+        internalTask();
+}}}}}
+public String startParameter(){
+    internalTask();
+    return mode;
 }
+
 }
-}
+
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Controller {
-
+    Arguments arguments;
     private String[] args;
     private ArrayList<String> jsonFoldersList;
 
@@ -27,28 +27,7 @@ public class Controller {
         OpenPoseManager openPoseManager = new OpenPoseManager(parameters);
 
         //створюємо NewVision менеджер, передаєм туди список папок і стартуєм
-
-        Scanner sc = new Scanner("");
-        //TODO: аргументы в отдельном классе. Если args[i] начинается с "-", то в args[i+1] должно содержаться соответсвуеще значение
-        if(args[0]==""){
-            System.out.println("No arguments found! Please, choose mode:");
-            String s = sc.nextLine();
-            if(s=="parallel"||s=="p"){
-                System.out.println("Starting parallel mode...");
-                openPoseManager.start();
-                newVisionManager.start();
-            }
-            if(s=="OpenPose"&&s=="op"){
-                System.out.println("Starting OpenPose only mode...");
-                openPoseManager.start();
-            }
-            if(s=="NewVision"&&s=="nv"){
-                System.out.println("Starting NewVision only mode...");
-                newVisionManager.start();
-            }
-            if(s=="q"&&s=="exit"){
-                System.exit(0);
-            }
-        }
+       arguments.setArgs(args);
+       arguments.startParameter();
     }
 }
