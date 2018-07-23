a4c68b79aeeb39e9fd649548b7c9cbde9956b9ac
radkn <radkn@users.noreply.github.com>
2018-07-23 20:22:25 +0300

    correct ScenePolygonParams in comparison to NewVision

--- src/main/java/imageProcessing/ScenePolygonParams.java
+++ src/main/java/imageProcessing/ScenePolygonParams.java
@@ -8,9 +8,14 @@ import java.util.*;
  */
 public class ScenePolygonParams {
 
-    public Polygon polygon = new Polygon(new int[]{0,0,0,0},new int[]{0,0,0,0},4);
+
+    public Polygon polygon;
     public String location = "DefaultLocation";
     public String name = "DefaultName"+((System.currentTimeMillis() ^ new Random().nextLong()) & 0xFFFF);
+
+    /*public ScenePolygon constructFrom(){
+        return new ScenePolygon(this);
+    }*/
 
     public java.util.List<Point> getPoints() {
 
@@ -25,23 +30,64 @@ public class ScenePolygonParams {
     }
 
     public void setPoints(java.util.List<Point> pArray) {
-        polygon = new Polygon();
-
-        int[] x = new int[pArray.size()];
-        int[] y = new int[pArray.size()];
+        polygon = new Polygon(new int[pArray.size()], new int[pArray.size()], pArray.size());
 
         int n=pArray.size();
+        polygon.npoints=n;
         for(int i=0; i<n; i++){
             Point p = pArray.get(i);
-            x[i] = p.x;
-            y[i] = p.y;
+            polygon.xpoints[i] = p.x;
+            polygon.ypoints[i] = p.y;
         }
-        polygon = new Polygon(x,y,n);
+    }
+
+    public void setNewPoint(int x, int y){
+        polygon.addPoint(x, y);
+    }
+
+    public int getNumberOfPoints(){
+        return polygon.npoints;
     }
 
     @Override
     public String toString(){
         return name;
     }
+
+//    public Polygon polygon = new Polygon(new int[]{0,0,0,0},new int[]{0,0,0,0},4);
+//    public String location = "DefaultLocation";
+//    public String name = "DefaultName"+((System.currentTimeMillis() ^ new Random().nextLong()) & 0xFFFF);
+//
+//    public java.util.List<Point> getPoints() {
+//
+//        int n=polygon.npoints;
+//        java.util.List<Point> pArray = new ArrayList<>();
+//        for(int i=0; i<n; i++){
+//            Point temp = new Point();
+//            temp.setLocation(polygon.xpoints[i],polygon.ypoints[i]);
+//            pArray.add(temp);
+//        }
+//        return pArray;
+//    }
+//
+//    public void setPoints(java.util.List<Point> pArray) {
+//        polygon = new Polygon();
+//
+//        int[] x = new int[pArray.size()];
+//        int[] y = new int[pArray.size()];
+//
+//        int n=pArray.size();
+//        for(int i=0; i<n; i++){
+//            Point p = pArray.get(i);
+//            x[i] = p.x;
+//            y[i] = p.y;
+//        }
+//        polygon = new Polygon(x,y,n);
+//    }
+//
+//    @Override
+//    public String toString(){
+//        return name;
+//    }
 
 }
